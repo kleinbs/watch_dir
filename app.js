52e8bd3574f956fa86efeba03266559b789b0061
kleinbs <kleinbs1234@gmail.com>
2015-04-22 18:06:12 -0700

    monitors a directory that is user specified for changes
    committing what I have

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 //test
+var watch = require('watch');
+var fs = require('fs');
+
+watch.createMonitor(process.argv[2], function (monitor) {
+	monitor.files['/home/mikeal/.zshrc'] // Stat object for my zshrc.
+	monitor.on("created", function (f, stat) {
+	  console.log("created " + f)// Handle new files
+	})
+	monitor.on("changed", function (f, curr, prev) {
+	  // Handle file changes
+	})
+	monitor.on("removed", function (f, stat) {
+	  // Handle removed files
+	})
+	//monitor.stop(); // Stop watching
+})
