e6a25cbb385bfd61de484d48b3246fdcb8f6c833
Klein, Brian (HAL) <BKlein@HollandAmerica.com>
2015-04-24 20:18:07 -0700

    adding some changes

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ var fs = require('fs');
 var through = require('through2');
 var path = require('path');
 
+//var buffer = through(start, end);
+
 
 var read = process.argv[2];
 var write = process.argv[3];
@@ -14,10 +16,10 @@ watch.createMonitor(read, function (monitor) {
 	monitor.on("created", function (f, stat) {
 		if(stat.isFile())
 			convertToJson(f);
-		console.log("created " + f)// Handle new files
 	})
 	monitor.on("changed", function (f, curr, prev) {
-	  // Handle file changes
+	  if(curr.isFile())
+			convertToJson(f);
 	})
 	monitor.on("removed", function (f, stat) {
 	  // Handle removed files
@@ -30,17 +32,37 @@ function convertToJson(f){
 	console.log(f);
 	var pathInfo = path.parse(f)
 	var parser = new xml2js.Parser()
+	var jsonString;
 
-	if(pathInfo.ext !== '.xml') return;
+	if(pathInfo.ext !== '.xml') return console.log("Not XML, no changes");
 	
-	var toXML = fs.readFile(f, function(err, data){
-		parser.parseString(data, function(err, result){
-			fs.writeFile(pathInfo.dir + "/" + pathInfo.name + ".json", result, function(err){
-				if(err){
-					console.log("Couldn't write JSON: " + e)
-					return
-				}
-			})
-		})
-	})
+	fs.createReadStream(f).pipe(through(write, end)).pipe(fs.createWriteStream(pathInfo.dir + "/" + pathInfo.name + ".json"));
+		//parser.parseString(data, function(err, result){
+		//	console.log(result);
+		//	var jsonString = result;
+		//	console.log(jsonString);
+		//	//fs.writeFile(pathInfo.dir + "/" + pathInfo.name + ".json", jsonString, function(err, data2){
+			//	console.log("inside " + jsonString);
+			//	if(err){
+			//		console.log("Couldn't write JSON: " + err)
+			//		return
+			//	}
+			//})
+
+	function write(buff, enc, next){
+
+		console.log("Incoming XML \n" + buff.toString());
+		this.push(parser.parseString(buff.toString(), function(err, result){
+			if(err) return console.log("there was an error");
+
+			return result;
+		}))
+	}
+
+	function end(done){
+		//this.push(jsonString);
+		done();
+	}
 }
+
+
