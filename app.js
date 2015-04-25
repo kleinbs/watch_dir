c0a8f2e1a8271da83e3e1428ad10dbc09b95b9aa
kleinbs <kleinbs1234@gmail.com>
2015-04-25 15:20:57 -0700

    Revert 13d6239..55d7f80
    This rolls back to commit 13d623915d0fcc2f4d5cb650d4f181e5d4c00ab7.

--- app.js
+++ app.js
@@ -4,15 +4,14 @@ var xml2js = require('xml2js');
 var fs = require('fs');
 var through = require('through2');
 var path = require('path');
-var mkdirp = require('mkdirp');
 
 //var buffer = through(start, end);
 
 
-var readRootDir = process.argv[2];
-var writeRootDir = process.argv[3];
+var read = process.argv[2];
+var write = process.argv[3];
 
-watch.createMonitor(readRootDir, function (monitor) {
+watch.createMonitor(read, function (monitor) {
 
 	monitor.on("created", function (f, stat) {
 		if(stat.isFile())
@@ -30,39 +29,22 @@ watch.createMonitor(readRootDir, function (monitor) {
 
 function convertToJson(f){
 
-	//console.log(f);
+	console.log(f);
 	var pathInfo = path.parse(f)
 	var parser = new xml2js.Parser()
 	var xmlFile = '';
 	var jsonString;
 
-	var outFileDir = (pathInfo.dir + '/').replace(readRootDir, writeRootDir) 
-	//console.log(readRootDir);
-	//console.log(pathInfo.dir)
-	//console.log(writeRootDir);
-	//console.log(outFile);
-	console.log(pathInfo.ext)
 	if(pathInfo.ext !== '.xml') return console.log("Not XML, no changes");
 	
-	mkdirp(outFileDir, function (err) {
-		if (err) return console.log("Couldn't create directory");
-		console.log('hi')
-		//console.log(f)
-		fs.createReadStream(f).pipe(through(read, done)).pipe(through(write, end))
-			.pipe(fs.createWriteStream(outFileDir + pathInfo.name + ".json")
-			.on('error', function(err){ console.log(err)}));
-			done();
-		console.log('done');
-	})	
+	fs.createReadStream(f).pipe(through(read, done)).pipe(through(write, end)).pipe(fs.createWriteStream(pathInfo.dir + "/" + pathInfo.name + ".json").on('error', function(err){ console.log(err)}));
 
 	function read(buff, enc, next){
-		console.log("talk to me " + buff.toString());
 		xmlFile += buff.toString();
 		next();
 	}
 
 	function done(done){
-		console.log("this is the xml file " + xmlFile);
 		this.push(xmlFile);
 		done();
 	}
@@ -71,7 +53,6 @@ function convertToJson(f){
 
 		parser.parseString(xmlFile, function(err, result){
 			if(err) return console.log("there was an error " + err);
-			console.log(result);
 			jsonString = result;
 			next();
 		})
@@ -79,22 +60,8 @@ function convertToJson(f){
 
 	function end(done){
 		this.push(JSON.stringify(jsonString));
-		console.log("writing JSON")
+		console.log("writing xml")
+		done();
 	}
-
-	function mkdir(dirPath, todo){
-		console.log(dirPath);
-		fs.mkdirParent = function(dirPath, mode, callback) {
-			console.log(dirPath);
-			fs.mkdir(dirPath, mode, function(error){
-				if(error && error.errno === 34){
-					console.log(dirPath)
-					fs.mkdirParent(path.dirname(dirPath), mode, callback);
-					fs.mkdirParent(path, mode, calback);
-				}
-				callback && callback(error);
-			});
-		};
-	};
 }
 
