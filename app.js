ea1aafe7df352dd9306ebff379d18f46d4926823
kleinbs <kleinbs1234@gmail.com>
2015-04-25 18:23:03 -0700

    added some new stuff

--- app.js
+++ app.js
@@ -4,14 +4,17 @@ var xml2js = require('xml2js');
 var fs = require('fs');
 var through = require('through2');
 var path = require('path');
+var mkdirp = require('mkdirp');
 
 //var buffer = through(start, end);
 
 
-var read = process.argv[2];
-var write = process.argv[3];
+var readRootDir = process.argv[2];
+var writeRootDir = process.argv[3];
 
-watch.createMonitor(read, function (monitor) {
+watch.createMonitor(readRootDir, function (monitor) {
+
+	convertExistingToJson(readRootDir)
 
 	monitor.on("created", function (f, stat) {
 		if(stat.isFile())
@@ -34,10 +37,18 @@ function convertToJson(f){
 	var parser = new xml2js.Parser()
 	var xmlFile = '';
 	var jsonString;
+	var outFileDir = (pathInfo.dir + '/').replace(readRootDir, writeRootDir) 
 
 	if(pathInfo.ext !== '.xml') return console.log("Not XML, no changes");
-	
-	fs.createReadStream(f).pipe(through(read, done)).pipe(through(write, end)).pipe(fs.createWriteStream(pathInfo.dir + "/" + pathInfo.name + ".json").on('error', function(err){ console.log(err)}));
+
+	console.log("attempting to convert " + pathInfo.base);
+
+	mkdirp(outFileDir, function (err) {
+		fs.createReadStream(f).pipe(through(read, done))
+			.pipe(through(write, end))
+			.pipe(fs.createWriteStream(outFileDir + pathInfo.name + ".json")
+				.on('error', function(err){ console.log(err)}));
+	});
 
 	function read(buff, enc, next){
 		xmlFile += buff.toString();
@@ -60,8 +71,20 @@ function convertToJson(f){
 
 	function end(done){
 		this.push(JSON.stringify(jsonString));
-		console.log("writing xml")
 		done();
 	}
+}
+
+function convertExistingToJson(rootDir){
+
+	fs.readdir(rootDir, function(err, files){
+		for (var i in files){
+			var stats = fs.lstatSync(rootDir + '/' + files[i]);
+			if(stats.isFile())
+				convertToJson(rootDir + "/" + files[i]);
+			else if(stats.isDirectory())
+				convertExistingToJson(rootDir + "/" + files[i])
+		}
+	})
 }
 
